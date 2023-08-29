861d94a91ec82feb9c0e8971f4d6722a5955c834
Mukhammad-Umar <yoqubjonov98@mail.ru>
2023-08-29 18:13:58 +0500

    api

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,16 +1,16 @@
 /*____________________ Server ____________________*/
-export const URL_BASE = '/';
-export const URL_IMG = '/files/';
+// export const URL_BASE = '/';
+// export const URL_IMG = '/files/';
 
 
 /*____________________ Test Global ____________________*/
-// export const URL_BASE = 'http://100.67.202.95:8000/'
-// export const URL_IMG = 'http://100.67.202.225:8000/files/'
+export const URL_BASE = 'http://100.67.202.95:8000/'
+export const URL_IMG = 'http://100.67.202.225:8000/files/'
 
 
 /*____________________ Test Local ____________________*/
-// export const URL_BASE = 'http://100.67.202.225:8000/'
-// export const URL_IMG = 'http://100.67.202.225:8000/files/'
+// export const URL_BASE = 'http://172.22.6.13:8216/'
+// export const URL_IMG = 'http://172.22.6.13:8216/files/'
 
 
 // export const LANG = navigator.language.substr(0, 2).toLowerCase() || 'ru';
