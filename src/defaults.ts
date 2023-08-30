898365d35098138b75f3f5ed8bd386906cca5a15
Mukhammad-Umar <yoqubjonov98@mail.ru>
2023-08-30 11:59:45 +0500

    sss

--- src/defaults.ts
+++ src/defaults.ts
@@ -4,13 +4,13 @@
 
 
 /*____________________ Test Global ____________________*/
-export const URL_BASE = 'http://100.67.202.95:8000/'
-export const URL_IMG = 'http://100.67.202.225:8000/files/'
+// export const URL_BASE = 'http://100.67.202.95:8000/'
+// export const URL_IMG = 'http://100.67.202.225:8000/files/'
 
 
 /*____________________ Test Local ____________________*/
-// export const URL_BASE = 'http://172.22.6.13:8216/'
-// export const URL_IMG = 'http://172.22.6.13:8216/files/'
+export const URL_BASE = 'http://172.22.6.13:8216/'
+export const URL_IMG = 'http://172.22.6.13:8216/files/'
 
 
 // export const LANG = navigator.language.substr(0, 2).toLowerCase() || 'ru';
